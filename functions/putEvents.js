52d7c709b96edc11f39424c91ab5ed624f1893b5
vusirikala <satyasatya123456@gmail.com>
2022-12-24 13:56:55 -0600

    Add an event bridge

--- functions/putEvents.js
+++ functions/putEvents.js
@@ -1,8 +1,29 @@
-
+const EventBridge = require("aws-sdk/clients/eventbridge");
+const EVENT_BUS_NAME = process.env.EventBusName;
+let eventBridge = new EventBridge();
 
 module.exports.putEvents = async (event) => {
     let body = JSON.parse(event.body);
-    
-    //put Events to EventBridge
-
+    console.log("Event bus name: ", EVENT_BUS_NAME)
+    let entry = {
+        EventBusName: EVENT_BUS_NAME,
+        Detail: JSON.stringify({
+            vehicleNumber: body.vehicleNumber,
+            NIC: body.nic
+        }),
+        Source: "fuel-app",
+        DetailType: "user-signup",
+    }
+    try {
+        let output = await eventBridge.putEvents({Entries: [entry]}).promise();
+        return {
+            statusCode: 200,
+            body: JSON.stringify(output)
+        }
+    } catch (err) {
+        return {
+            statusCode: 500,
+            body: err.message
+        }
+    }
 }
