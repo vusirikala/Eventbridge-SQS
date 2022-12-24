fbaa65eebd23ab96cda28fcff8154c49f199697b
vusirikala <satyasatya123456@gmail.com>
2022-12-24 17:27:43 -0600

    Add dead letter queue

--- functions/processEvents.js
+++ functions/processEvents.js
@@ -1,16 +1,19 @@
 
 
 module.exports.processEvents = async (event) => {
-    let records = event.records;
+    let records = event.Records;
     let batchItemFailures = [];
 
     if (records.length) {
         for (const record of records) {
             try {
                 const parsedBody = JSON.parse(record.body);
+                
+                if (typeof parsedBody.detail.vehicleNumber !== 'string') {
+                    throw new Error("Vehicle number must be string")
+                }
                 console.log("Processing record ", record)
                 console.log("Process vehicle details" , parsedBody)
-
             } catch (err) {
                 batchItemFailures.push({
                     itemIdentifer: record.messageId
